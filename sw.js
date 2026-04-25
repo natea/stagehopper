bd869a2fcd1e5fcfdcd0f9851bc624dfc47fe0fd
Nate Aune <natejaune@gmail.com>
2026-04-25 11:51:59 -0500

    Bump SW to v7 to force cache refresh

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE_VERSION whenever you ship updates so users get fresh files.
-const CACHE_VERSION = 'jazzfest-v6';
+const CACHE_VERSION = 'jazzfest-v7';
 const CORE = [
   './',
   './index.html',
